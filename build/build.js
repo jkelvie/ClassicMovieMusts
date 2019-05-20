9e838db931af52e08396132f392f1cf2d34c769c
John Kelvie <jpk@bespoken.io>
2019-05-20 17:32:08 -0500

    Lots of work on deployment stuff

---
+++ build/build.js
@@ -0,0 +1,34 @@
+require("dotenv").config({ path: "dev.env" });
+const shelljs = require("shelljs");
+
+if (process.env.CLEAN) {
+    shelljs.rm("-rf", "target");
+}
+
+shelljs.mkdir("-p", "target");
+// Setup the ASK config
+shelljs.mkdir("target/.ask");
+shelljs.cp("-f", "../.ask/config", "target/.ask/config");
+
+shelljs.sed("-i", "FUNCTION_NAME", process.env.FUNCTION_NAME, "target/.ask/config");
+shelljs.sed("-i", "LAMBDA_ARN", process.env.LAMBDA_ARN, "target/.ask/config");
+shelljs.sed("-i", "SKILL_ID", process.env.SKILL_ID, "target/.ask/config");
+
+// Copy over the model
+shelljs.mkdir("target/models");
+shelljs.cp("-f", "../models/*", "target/models");
+
+shelljs.sed("-i", "INVOCATION_NAME", process.env.INVOCATION_NAME, "target/models/en-US.json");
+
+// Copy over the skill.json
+shelljs.cp("-f", "../skill.json", "target");
+
+shelljs.sed("-i", "FUNCTION_NAME", process.env.FUNCTION_NAME, "target/skill.json");
+shelljs.sed("-i", "SKILL_NAME", process.env.SKILL_NAME, "target/skill.json");
+
+// Copy all the other files we need
+shelljs.cp("../package.json", "target/");
+shelljs.cp("-r", "../lib", "target/");
+shelljs.cp("-r", "../hooks", "target/"); // If we don't copy the hooks, they will be created anew
+shelljs.exec("cd target && npm install --only=prod");
+
--- hooks/pre_deploy_hook.ps1
+++ hooks/pre_deploy_hook.ps1
@@ -16,39 +16,4 @@ param(
     [string] $TARGET = "all"
 )
 
-function install_dependencies ($CWD, $SOURCE_DIR) {
-    Set-Location $SOURCE_DIR
-    Invoke-Expression "npm install" 2>&1 | Out-Null
-    $EXEC_RESULT = $?
-    Set-Location $CWD
-    return $EXEC_RESULT
-}
-
-if ($DO_DEBUG) {
-    Write-Output "###########################"
-    Write-Output "##### pre-deploy hook #####"
-    Write-Output "###########################"
-}
-
-if ($TARGET -eq "all" -Or $TARGET -eq "lambda") {
-    $ALL_SOURCE_DIRS = Get-Content -Path "skill.json" | select-string  -Pattern "sourceDir" -CaseSensitive
-    Foreach ($SOURCE_DIR in $ALL_SOURCE_DIRS) {
-        $FILTER_SOURCE_DIR = $SOURCE_DIR -replace "`"", "" -replace "\s", "" -replace ",","" -replace "sourceDir:", ""
-        $CWD = (Get-Location).Path
-        if (install_dependencies $CWD $FILTER_SOURCE_DIR) {
-            if ($DO_DEBUG) {
-                Write-Output "Codebase ($FILTER_SOURCE_DIR) built successfully."
-            }
-        } else {
-            if ($DO_DEBUG) {
-                Write-Output "There was a problem installing dependencies for ($FILTER_SOURCE_DIR)."
-            }
-            exit 1
-        }
-    }
-    if ($DO_DEBUG) {
-        Write-Output "###########################"
-    }
-}
-
 exit 0
