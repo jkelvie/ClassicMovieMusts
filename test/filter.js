6c916d09f926839de40ccc646b8d264093ee9ca4
John Kelvie <jpk@bespoken.io>
2019-10-20 10:03:13 -0400

    Fixed handling of intents when waiting for an answer
    Fixed test for AudioPlayer on latest episode
    Added filter to resolve latest episode

---
+++ test/filter.js
@@ -0,0 +1,21 @@
+const RSS = require("../lib/rss");
+
+let feed;
+
+module.exports = {
+    resolve: async (variable) => {
+        // interaction allows seeing any information from the interaction
+        // and the parent test and testSuite you need
+        if (!feed) {
+            const rss = new RSS();
+            console.log("FILTER LOADING FEED");
+            feed = await rss.fromURL("https://feed.podbean.com/www.classicmoviemusts.com/feed.xml");
+        }
+
+        if (variable === "latestEpisode") {
+            const episode = feed.podcasts[0].episode;
+            console.log("FILTER LATEST EPISODE: " + episode);
+            return episode;
+        }
+    },
+};
